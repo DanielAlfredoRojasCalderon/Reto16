af3b33d363a1ebebe6de7e9d29835d97d378fd79
Daniel Rojas <pe@unmsm.edu.pe>
2025-03-10 23:20:40 -0500

    Origen

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import First from "./FirstPart/First"
+import React from 'react'
+
+
+
+function App() {
+
+  return (
+    <div className="h-full ">
+      <First />
+
+    </div>
+    
+  )
+}
+
+export default App
